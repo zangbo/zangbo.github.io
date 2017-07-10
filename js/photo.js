9e1fb58cfe3e4cf570adf1f7fe27d6b2f963935e
zangbo <zb9397@sina.com>
2017-07-10 21:07:59 +0800

    Site updated: 2017-07-10 21:07:58

---
+++ js/photo.js
@@ -0,0 +1,10 @@
+var render = function(page, data){
+	var img = "";
+	for (var i = 0; i < data.length; i++) {
+		img += '<li><div class="img-box">' + '<a class="img-bg " rel="example_group" data-fancybox="images" href="http://orwbuystz.bkt.clouddn.com/' + data[i] + '"></a>' + '<img lazy-src="http://orwbuystz.bkt.clouddn.com/' + data[i] + '" />'  + '</div></li>';
+		//img += '<img src="http://yourqiniu.url.com/' + data[i] + '" />';
+	}
+	$(".img-box-ul").append(img);
+	$(".img-box-ul").lazyload();
+	$("a[rel=example_group]").fancybox();
+}
