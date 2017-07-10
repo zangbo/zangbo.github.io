391a6bb3d32ce7d1a4305681b64ef7045ec3a8fc
zangbo <zb9397@sina.com>
2017-07-11 01:09:04 +0800

    Site updated: 2017-07-11 01:09:03

--- photos/photos.js
+++ photos/photos.js
@@ -1,7 +1,7 @@
 var render = function(page, data){
 	var img = "";
 	for (var i = 0; i < data.length; i++) {
-		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://orwbuystz.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://orwbuystz.bkt.clouddn.com/' + data[i] + '" src="http://orwbuystz.bkt.clouddn.com/' + data[i] + '" style>'  + '</div></li>';
+		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://orwbuystz.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://orwbuystz.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
 			//img += '<img src="http://7xodcg.com1.z0.glb.clouddn.com/' + data[i] + '">';
 	}
 	$(".img-box-ul").append(img);
